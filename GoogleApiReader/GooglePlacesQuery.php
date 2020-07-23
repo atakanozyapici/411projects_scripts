975a487ee13145d21736e08e78c35d0cb2b512dc
unknown <animating101@gmail.com>
2020-07-24 00:10:30 +0300

    BUG: Can't see PlacesApi

---
+++ GoogleApiReader/GooglePlacesQuery.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace vendor\SKAgarwal\GoogleApi\PlacesApi;
+
+use SKAgarwal\GoogleApi\PlacesApi;
+
+class GooglePlacesQuery
+{
+    private $places;
+
+    function __constructor(){
+        $places = new PlacesApi('AIzaSyC-RXf36R3F1uCFAMnAsEMdrn1BW9z24L8');
+        echo "Butt";
+    }
+}
+
+?>
---
+++ GoogleApiReader/test_query_places.php
@@ -0,0 +1,16 @@
+<?php
+// Works.
+//echo var_dump(@include_once('/GoogleApiReader/GooglePlacesQuery.php')); // 'http://localhost/GooglePlacesQuery.php?from=eatery&where1=Eatery_Name&where2=Baban';
+//$places = new GooglePlacesQuery();
+
+namespace SKAgarwal\GoogleApi\PlacesApi;
+
+if ( ! class_exists('PlacesApi'))
+    echo 'wtf';
+else
+    echo 'HAHA! >:D';
+
+//$places = new PlacesApi('AIzaSyC-RXf36R3F1uCFAMnAsEMdrn1BW9z24L8');
+
+
+?>
