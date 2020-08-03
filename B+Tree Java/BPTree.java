ddb53162e990ea488b542dde79ac12940547444e
unknown <animating101@gmail.com>
2020-08-03 16:47:17 +0300

    Insertion on B+Tree working

--- B+Tree Java/BPTree.java
+++ B+Tree Java/BPTree.java
@@ -1,181 +1,341 @@
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BPTree {
-    
-    public class BPNode {
+    public int g_maxKeys;
+    public int g_maxChild;
+    public BPNode m_rootNode; 
+
+    protected class BPNode {
+        // list of keys
         public int[] m_keys;
+        // list of child nodes
         public BPNode[] m_children;
-        public BPNode m_linkedNode;
-        public int m_maxKeys;
-        protected int m_keyCount;
-        protected int m_childCount;
-        public boolean m_isLeaf;
-        public boolean m_isRoot;
+        // only applicable for leaves
+        public String[] m_values;
         public BPNode m_parent;
+        public int m_childCount;
+        public int m_keyCount;
+        public BPNode m_linkedNode;
 
-        
-        public BPNode(final int maxKeys) {
-            m_maxKeys = maxKeys;
-            m_keys = new int[maxKeys + 1];
-            m_children = new BPNode[maxKeys + 2];
+        public boolean isLeaf() {
+            return m_childCount == 0;
+        }
+        public boolean isRoot() {
+            if(m_parent == null && m_rootNode != this) {
+                System.out.println("no parent, but isn't recorded as root");
+            }
+            return m_parent == null;
+        }
+
+        public BPNode(int key) {
+            m_keys = new int[g_maxKeys + 1];
+            m_children = new BPNode[g_maxChild + 1];
+            m_childCount = 0;
             m_keyCount = 0;
-            m_childCount = 0;   
+
+            m_keys[m_keyCount] = key;
+            m_keyCount++;
         }
 
-        // For non-root nodes
-        public boolean validNode(){
-            return (m_isRoot && m_keyCount > 0) || (m_keyCount >= m_maxKeys/2 && (m_childCount == m_keyCount + 1 || m_isLeaf));
+        public BPNode() {
+            m_keys = new int[g_maxKeys + 1];
+            m_children = new BPNode[g_maxChild + 1];
+            m_childCount = 0;
+            m_keyCount = 0;
         }
 
-        // For leaf nodes, to check if an insert would require a shuffle
-        public boolean insertWithoutProblem() {
-            return m_keyCount == m_maxKeys;
+        public boolean keyOverflow() {
+            return m_keyCount > g_maxKeys;
         }
 
-        // For leaf nodes, to check if an insert would require a shuffle
-        public boolean deleteWithoutProblem() {
-            return m_keyCount > m_maxKeys/2;
-        }
-    
-        // Return false if overflow, true otherwise
-        public boolean insertKey(final int newKey) {
-            // If the node isn't full, we can follow a simple process
-            for(int i = 0; i < m_keyCount; i++) {
-                if(m_keys[i] == newKey) {
-                    return true;
-                } else if(m_keys[i] > newKey) {
-                    for(int j = m_keyCount; j > i; j--) {
-                        m_keys[j] = m_keys[j-1];
-                    }
-                    m_keys[i] = newKey;
-                    m_keyCount++;
-                    return true;
-                }
+        // Returns true on successful delete, false on failure
+        public boolean deleteKey(int key) {
+            BPNode node = searchKey(key);
+            if(node == null) {
+                return false;
             }
 
-            // if code reaches this point, that means the new key needs to be inserted at the tail end
-            m_keys[m_keyCount] = newKey;
-            m_keyCount++;
 
-            if(m_keyCount == m_maxKeys) 
-                return false;
-            else
-                return true;
+            return true;
 
         }
 
-        protected BPNode splitNode() {
-            BPNode newNode = new BPNode(m_maxKeys);
+        // Returns true on a successful insert, and false on failure
+        public boolean insertKey(int key) {
+            if(!isLeaf()) {
+                BPNode insertTo = searchNode(key);
+                if(insertTo == null) {
+                    System.out.println("Insert key " + String.valueOf(key) + " into: null for some reason");
+                } else {
+                    System.out.println("Insert key " + String.valueOf(key) + " into: " + insertTo.toString());
+                }
 
+                if(insertTo == null) {
+                    return false;
+                }
+
+                return insertTo.insertKey(key);
+            }
+
+            int insertIdx = sortedInsert(key);
+
+            // If no overflow, return successfully
+            if(!keyOverflow()) {
+                return true;
+            }
+            System.out.println(String.valueOf(key) + " overflows");
+
+            splitNode();
+
+            return true;
+        }
+
+
+        protected BPNode splitNode() {
+            // Create new node
+            BPNode newNode = new BPNode();
+
+            // Set parent, and linked node
             newNode.m_parent = m_parent;
             newNode.m_linkedNode = m_linkedNode;
             m_linkedNode = newNode;
+            
+            // Create index to parse newNode's keys and children
             int idx = 0;
-            m_keyCount = m_maxKeys/2;
 
-            for(int i = m_maxKeys/2; i < m_maxKeys; i++) {
+            // If not a leaf node, then don't copy over the middle key, it will be propagated upward
+            int keyStartIdx = getSplitIdx();
+            // Store the key that will be used to insert to the parent
+            int insertKey = m_keys[(g_maxKeys + 1)/2];
+
+            boolean isLeaf = isLeaf();
+            // Copy keys and children
+            for(int i = keyStartIdx; i <= g_maxKeys; i++) {
                 newNode.m_keys[idx] = m_keys[i];
+                newNode.m_children[idx] = m_children[i + 1];
 
+                // increment newNode key index
                 idx++;
+                // Reduce # of keys each time a key is transferred to newNode
                 m_keyCount--;
-            }
-
-            return newNode;
-        }
-
-
-        // HELPERS
-        public BPNode findNode(final int val) {
-            if(m_isLeaf) 
-                return this;
-
-            for(int i = 0; i < m_keyCount; i++) {
-                if(m_keys[i] > val) {
-                    return m_children[i].findNode(val);
+                
+                if(!isLeaf) {
+                    m_childCount--;
+                    newNode.m_childCount++;
                 }
             }
+            // Add last child to the newNode
+            if(!isLeaf) {
+                newNode.m_children[idx] = m_children[g_maxKeys + 1];
+                newNode.m_childCount++;
+            }
 
-            return m_children[m_keyCount].findNode(val);
-            
+            // Set newNode's key count
+            newNode.m_keyCount = idx;
+
+            if(!isLeaf) {
+                m_keyCount--;
+                m_childCount--;
+            }
+
+            if(isRoot()) {
+                BPNode[] arr = {this, newNode};
+                return createParent(arr, insertKey);
+            }
+
+            return m_parent.insertChild(newNode, insertKey);
         }
 
-        /// Finds the index of the value, or the index of the smallest key greater than the value
-        public int findValIndex(final int val) {
+        // Creates a parent to split into
+        public BPNode createParent(BPNode[] children, int insertKey) {
+            m_rootNode = new BPNode(insertKey);
+            for(int i = 0; i < children.length; i++) {
+                m_rootNode.m_children[i] = children[i];
+                m_rootNode.m_childCount++;
+
+                children[i].m_parent = m_rootNode;
+            }
+            return m_rootNode;
+        }
+
+        // Inserts a child at a given index
+        public BPNode insertChild(BPNode newChild, int insertKey) {
+            newChild.m_parent = this;
+            int insertIdx = sortedInsert(insertKey);
+
+            m_children[insertIdx] = newChild;
+            m_childCount++;
+
+            if(keyOverflow()) {
+                return splitNode();
+            }
+
+            return this;
+        }
+
+        // Get the index at which the split node will start
+        protected int getSplitIdx() {
+            return isLeaf() ? (g_maxKeys + 1)/2 : (g_maxKeys + 1)/2 + 1;
+        }
+
+        // Inserts new key and appropriately increments keyCount, and sorts m_keys, returns the key's index
+        private int sortedInsert(int key) {
+            int insertIdx = m_keyCount;
+            // Find where the key should be inserted
             for(int i = 0; i < m_keyCount; i++) {
-                if(val <= m_keys[i]) {
+                if(m_keys[i] == key) {
                     return i;
+                } else if(m_keys[i] > key) {
+                    insertIdx = i;
+                    break;
                 }
             }
 
-            return -1;
+            // shift keys to make room for the new key
+            for(int i = m_keyCount; i > insertIdx; i--) {
+                m_keys[i] = m_keys[i - 1];
+            }
+
+            // insert new key
+            m_keys[insertIdx] = key;
+            m_keyCount++;
+
+            // if not a leaf node shift children as well to make room for new child insert
+            // NOTE: Does not increment child count. Caller function needs to take care of insertion
+            if(!isLeaf()) {
+                for(int i = m_childCount - 1; i > insertIdx; i--) {
+                    m_children[i + 1] = m_children[i];
+                }
+            }
+
+            return insertIdx + 1;
         }
 
-    }
+        // Searches a key's node in a tree. If found, return null, and if not found, return the node it could be added to
+        public BPNode searchNode(int key) {
+            if(isLeaf()) {
+                for(int i = 0; i < m_keyCount; i++) {
+                    if(m_keys[i] == key) {
+                        System.out.println("Insert key " + String.valueOf(key) + " found in " + this.toString());
+                        
+                        return null;
+                    }
+                }
 
-    public BPNode m_rootNode;
+                // If key is not present, return null
+                return this;
+            }
 
-    public BPTree(final int maxKeys) {
-        m_rootNode = new BPNode(maxKeys);
-        m_rootNode.m_isRoot = true;
-    }    
+            for(int i = 0; i <= m_keyCount; i++) {
+                if(i == m_keyCount || m_keys[i] > key) {
+                    return m_children[i].searchNode(key);
+                } 
+            }
 
-    public boolean InsertKey(int key) {
-        if(m_rootNode.m_childCount < 2) {
-            return false;
+            return m_children[m_childCount - 1].searchKey(key);
         }
-
-        BPNode insertTo = findNode(key);
-        return false;
-    }
-
-    public int[] findKeys(final int minVal, final int maxVal) {
-        final BPNode minValNode = findNode(minVal);
-        BPNode parseNode = minValNode;
-        if(parseNode == null) {
-            return new int[0];
-        }
-
-        final int startIdx = parseNode.findValIndex(minVal);
-        int idx = startIdx;
-        int curKey = parseNode.m_keys[idx];
-
-        int count = 0;
         
-        while(curKey <= maxVal) {
-            count++;
-            idx++;
+        // Searches a key in the tree, and if the key is found, returns the leaf node that contains it
+        public BPNode searchKey(int key) {
+            if(isLeaf()) {
+                for(int i = 0; i < m_keyCount; i++) {
+                    if(m_keys[i] == key) {
+                        return this;
+                    }
+                }
 
-            if(idx >= parseNode.m_maxKeys) {
-                parseNode = parseNode.m_linkedNode;
-                idx = 0;
+                // If key is not present, return null
+                return null;
             }
 
-            if(parseNode == null) {
-                break;
+            for(int i = 0; i < m_keyCount; i++) {
+                if(m_keys[i] > key) {
+                    return m_children[i].searchKey(key);
+                } 
             }
-            
-            curKey = parseNode.m_keys[idx];
-        }
 
-        parseNode = minValNode;
-        idx = startIdx;
-        final int[] keys = new int[count];
+            return m_children[m_childCount - 1].searchKey(key);
+        } 
 
-        for(int i = 0; i < count; i++) {
-            keys[i] = parseNode.m_keys[idx];
 
-            idx++;
 
-            if(idx >= parseNode.m_maxKeys) {
-                parseNode = parseNode.m_linkedNode;
-                idx = 0;
+        // Returns the string representation of this B+ Tree node.
+        @Override
+        public String toString() { 
+            String retStr = "/// ";
+            for(int i = 0; i < m_keyCount; i++) {
+                retStr += String.valueOf(m_keys[i]) + ", ";
             }
-        }
-
-        return keys;
+            retStr += " | ";
+            retStr += " childCount : " + String.valueOf(m_childCount) + ", keyCount : " + String.valueOf(m_keyCount) + " /// ";
+            return retStr; 
+        } 
     }
 
-    private BPNode findNode(final int val) {
-        return m_rootNode.findNode(val);
+    // Creates a new BPTree, and a root node with a key
+    public BPTree(int key, int maxKeys) {
+        g_maxKeys = maxKeys;
+        g_maxChild = maxKeys + 1;
+
+        m_rootNode = new BPNode(key);
     }
+
+    // Creates a new BPTree, and a root node with a key
+    public BPTree(int maxKeys) {
+        g_maxKeys = maxKeys;
+        g_maxChild = maxKeys + 1;
+    }
+
+    public void InsertKey(int key) {
+        if(m_rootNode == null) {
+            m_rootNode = new BPNode(key);
+        } else {
+            m_rootNode.insertKey(key);
+        }
+    }
+
+    // Prints tree
+    public String printTree() {
+        String tree = "";
+
+        Queue<BPNode> printQueue = new LinkedList<BPNode>();
+        Queue<Integer> printQueueLevel = new LinkedList<Integer>();
+
+        printQueue.add(m_rootNode);
+        printQueueLevel.add(0);
+
+        int lastPrintedLevel = 0;
+
+        while(!printQueue.isEmpty()) {
+            BPNode curNode = printQueue.remove();
+            int curLevel = printQueueLevel.remove();
+
+            for(int i = 0; i < curNode.m_childCount; i++) {
+                if(curNode.m_children[i] != null) {
+                    printQueue.add(curNode.m_children[i]);
+                    printQueueLevel.add(curLevel + 1);
+                }
+            }
+            if(curLevel > lastPrintedLevel) {
+                tree += String.valueOf(lastPrintedLevel);
+                tree += "\n";
+                lastPrintedLevel = curLevel;
+            }
+
+            tree += curNode.toString();
+
+        }
+        tree += String.valueOf(lastPrintedLevel);
+        tree += "\n";
+        return tree;
+    } 
+
+    @Override
+    public String toString() { 
+        String ret = "Key Capacity: " + String.valueOf(g_maxKeys) + ", Child Capacity: " + String.valueOf(g_maxChild) + "\n";
+        if(m_rootNode != null)
+            ret += m_rootNode.toString();
+        return ret;
+    } 
 }
